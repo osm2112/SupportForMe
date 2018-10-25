326b3c8485185708c4d8e0adbd20c4617066050f
OhSangMin <OhSangMin@DESKTOP-NBAL2GQ>
2018-10-25 23:53:25 +0900

    Hell of git 자꾸 부분부분 수정한거 날라 간다

--- SupportForMe/src/main/java/com/supportforme/biz/member/impl/MemberServiceImpl.java
+++ SupportForMe/src/main/java/com/supportforme/biz/member/impl/MemberServiceImpl.java
@@ -2,12 +2,15 @@ package com.supportforme.biz.member.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.supportforme.biz.member.MemberDTO;
 import com.supportforme.biz.member.MemberSerachDTO;
 import com.supportforme.biz.member.MemberService;
 import com.supportforme.biz.test.UserDTO;
 import com.supportforme.biz.test.UserSearchDTO;
 
+@Service
 public class MemberServiceImpl implements MemberService {
 
 	@Override
@@ -35,7 +38,7 @@ public class MemberServiceImpl implements MemberService {
 	}
 
 	@Override
-	public List<UserDTO> getMembers(MemberSerachDTO serachDTO) {
+	public List<MemberDTO> getMembers(MemberSerachDTO serachDTO) {
 		// TODO Auto-generated method stub
 		return null;
 	}
