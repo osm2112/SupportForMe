e201361c3360ed4b9ef1e6714dcb662c6ccd8bc5
OhSangMin <OhSangMin@DESKTOP-NBAL2GQ>
2018-10-25 17:11:45 +0900

    2018-10-25 오상민

--- SupportForMe/src/main/java/com/supportforme/biz/member/MemberDTO.java
+++ SupportForMe/src/main/java/com/supportforme/biz/member/MemberDTO.java
@@ -1,5 +1,77 @@
 package com.supportforme.biz.member;
 
 public class MemberDTO {
-
+	private	String userId;
+	private	String password;
+	private	String name;
+	private	String address;
+	private	String phoneNum;
+	private	String email;
+	private	String introduction;
+	private	String postcode;
+	private	String addr_detail;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public String getAddr_detail() {
+		return addr_detail;
+	}
+	public void setAddr_detail(String addr_detail) {
+		this.addr_detail = addr_detail;
+	}
+	@Override
+	public String toString() {
+		return "MemberDTO [userId=" + userId + ", password=" + password + ", name=" + name + ", address=" + address
+				+ ", phoneNum=" + phoneNum + ", email=" + email + ", introduction=" + introduction + ", postcode="
+				+ postcode + ", addr_detail=" + addr_detail + "]";
+	}
+	
+	
+	
+	
 }
--- SupportForMe/src/main/java/com/supportforme/biz/member/MemberService.java
+++ SupportForMe/src/main/java/com/supportforme/biz/member/MemberService.java
@@ -1,5 +1,24 @@
 package com.supportforme.biz.member;
 
+import java.util.List;
+
+import com.supportforme.biz.test.UserDTO;
+import com.supportforme.biz.test.UserSearchDTO;
+
 public interface MemberService {
 
+	//등록
+	public int insertMember(MemberDTO dto);
+	//수정
+	public int updateMember(MemberDTO dto);
+	//삭제(회원 탈퇴)
+	public int deleteMember(MemberDTO dto);
+	//단건 조회
+	public UserDTO getMember(MemberDTO dto);
+	
+	//전체 조회
+	public List<UserDTO> getMembers(UserSearchDTO serachDTO);
+	
+	public int getCnt(UserSearchDTO searchDTO); 
+	
 }
