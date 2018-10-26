02fe631d384d41e50cf2d65d108f5575e27d0b38
User <User@YD02-07>
2018-10-26 17:37:36 +0900

    1026 김효정

---
+++ SupportForMe/src/main/java/com/supportforme/biz/project/ProjectDetailPageService.java
@@ -0,0 +1,25 @@
+package com.supportforme.biz.project;
+import java.util.List;
+
+import com.supportforme.biz.comments.CommentsDTO;
+import com.supportforme.biz.hashtag.HashtagDTO;
+import com.supportforme.biz.present.PresentDTO;
+
+public interface ProjectDetailPageService {
+	
+	//프로젝트 기본정보 조회
+	public ProjectDTO getProjectDetail(ProjectDTO dto);
+	
+	//프토젝트가 포함된 해시태그 조회
+	public List<HashtagDTO> getProjectHashtags(ProjectDTO dto);
+	
+	//프로젝트의 리워드 목록 조회
+	public List<PresentDTO> getProjectPresents(ProjectDTO dto);
+
+	//프로젝트에 달린 댓글 조회
+	public List<CommentsDTO> getComments(ProjectDTO dto);
+	
+	//프로젝트 댓글에 달린 답글 조회
+	public List<CommentsDTO> getReplyComments(CommentsDTO dto);
+	
+}
